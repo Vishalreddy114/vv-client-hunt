6e57285adf6d5a8323ffcaa0d4778605273fb042
vishal Reddy <vishalreddy0507@gmail.com>
2021-01-28 01:14:23 -0600

    updated index.html, styles, and init-locations.js

--- init-locations.js
+++ init-locations.js
@@ -1,13 +1,17 @@
 export default [
-   {
-        Name: 'Horizons West Apartments',
-        Latitude: 40.358970,
-        Longitude: -94.883195
-    },
+//    {
+//         Name: 'Horizons West Apartments',
+//         Latitude: 40.358970,
+//         Longitude: -94.883195
+//     },
     {
         Name: 'Owens Library',
         Latitude:40.353784, 
         Longitude: -94.885906
+    },
+    {
+        Name: 'Student Union',
+        Latitude:100.353784, 
+        Longitude: -90.885906
     }
-    
 ];
