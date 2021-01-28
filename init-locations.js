fa6e29575db40635807bbd10500481f4b06f4be5
vishal Reddy <vishalreddy0507@gmail.com>
2021-01-27 21:39:20 -0600

    updated location.js, main.js. index.html, and init-locations.js

---
+++ init-locations.js
@@ -0,0 +1,13 @@
+export default [
+   {
+        Name: 'Horizons West Apartments',
+        Latitude: 40.358970,
+        Longitude: -94.883195
+    },
+    {
+        Name: 'Owens Library',
+        Latitude:40.353784, 
+        Longitude: -94.885906
+    }
+    
+];
