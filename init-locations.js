93f0c92638f55cf7ecc89beaab5da2dba0e22b9f
vishal Reddy <vishalreddy0507@gmail.com>
2021-02-01 16:05:10 -0600

    added locations

--- init-locations.js
+++ init-locations.js
@@ -8,5 +8,16 @@ export default [
         Name: 'Horizons West Apartments',
         Latitude: 40.355959,
         Longitude: -94.881454
+    },
+    {
+        Name: 'BD Owens Library',
+        Latitude: 40.35363302665877, 
+        Longitude: -94.885997840
+    },
+    {
+        Name: 'Hughes Field House',
+        Latitude: 40.35182197570809,  
+        Longitude: -94.88905369873045
     }
+
 ];
